7e0e575112ad4e362d899e16fdae32fb663d431c
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-02 17:31:33 -0500

    convert number to string and vice versa

---
+++ 26_convert_numbers_to_string_vice_versa.rb
@@ -0,0 +1,19 @@
+# string to numbers
+str = '5'
+p str.class
+
+p str.to_i(10)
+p str.to_f
+
+# numbers to string
+
+number = 42
+
+p number.to_s
+
+# float to int and string
+
+float_number = 4.5698
+
+p float_number.to_i
+p float_number.to_s
