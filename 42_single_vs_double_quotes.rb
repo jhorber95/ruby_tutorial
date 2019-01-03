e3d11fef498dfd32e50d2fbd591f1383e4ff09f0
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-03 15:36:51 -0500

    single quotes vs double quotes

---
+++ 42_single_vs_double_quotes.rb
@@ -0,0 +1,6 @@
+puts "I'm 25 \n year old"
+puts 'I\'m 25 \n year old' # dont work line break
+
+word = 'father'
+puts "I'm your #{word}"
+puts 'I\'m your #{word}' # interpolation dont work in single quotes
