de27144f1cab28911eb5183bef44ae66dbccb5ee
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-03 14:59:03 -0500

    the upto and downto methods

---
+++ 37_the_.upto_and_.downto.rb
@@ -0,0 +1,11 @@
+# 5.downto(1) { |index| p "countdown #{index}" }
+
+# 3.downto(0) do |index|
+#   puts "countdown #{index}"
+# end
+
+# -------------- upto method ------------------
+
+3.upto(6) do |i|
+  puts "countdown #{i}"
+end
