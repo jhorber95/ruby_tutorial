0d58963b24c7040d4b8d0311af283851b75e50a3
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-03 15:52:48 -0500

    Equality an Inequality operator with string

--- 43_equality_and_inequality_strings.rb
+++ 43_equality_and_inequality_strings.rb
@@ -4,3 +4,13 @@ c = "Hello"
 
 p a != "hi"
 p a == c
+
+# -----------------
+p 'apple' < 'kiwi'
+# rails appear before that ruby, alphabetical classification
+p 'ruby' < 'rails'
+
+# ---------- Lowercase and uppercase
+puts
+p "A" < "a"
+p "Z" < "a"
