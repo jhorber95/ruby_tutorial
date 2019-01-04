ae493fea75c85c83757117bd12adc26638879c4a
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-04 09:45:58 -0500

    override characters in strings with bracket syntax

---
+++ 49_override_character.rb
@@ -0,0 +1,10 @@
+words = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
+words[0, 4] = 'EDITED'
+p words
+
+words[20..30] = 'ANOTHER TEXT'
+p words
+
+words[7...15] = 'OTHER TEXT'
+p words
