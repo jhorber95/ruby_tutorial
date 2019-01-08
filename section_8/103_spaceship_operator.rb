f4e3fa564e177ffe02e46c059f4ca7ed8a624da6
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-08 15:47:15 -0500

    the spaceship operator

--- section_8/103_spaceship_operator.rb
+++ section_8/103_spaceship_operator.rb
@@ -1,3 +1,8 @@
+# possible output
+# 1 when the left side is greater than right side
+# -1 when the right side is greater than left side
+# 0 if two values on both sides of spaceship operator are equal
+# nil if the values are incomparable
 p 3 <=> 2
 p 5 <=> 10
 p 6 <=> 6
