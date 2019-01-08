0ac78774e8ead39ec3a58744c2b0ed8c9b9c883a
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-08 14:25:54 -0500

    the length, count and size methods on an array

--- section_8/96_length_size_arrays.rb
+++ section_8/96_length_size_arrays.rb
@@ -2,3 +2,7 @@ days = %w[monday tuesday wednesday thursday friday saturday sunday]
 
 p days.length
 p days.size
+p days.count
+
+# the count method print how many times appears a item
+p [12, 2, 5, 4, 8, 5, 5, 2, 2, 5, 8].count(5)
