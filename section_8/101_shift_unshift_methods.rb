99b79229b269ab3db16bae3fc6a7f007bb35480a
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-08 15:21:46 -0500

    the shift and unshift method

---
+++ section_8/101_shift_unshift_methods.rb
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 56, 9, 7]
+p numbers
+
+# remove first element
+numbers.shift
+p numbers
+
+puts
+
+# add items in the beginning of an array
+numbers.unshift(10..15)
+p numbers
