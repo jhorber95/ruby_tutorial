638414d21633f4a3724cbeedb5ef74afe459e960
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-08 14:17:33 -0500

    override one or more array elements

--- section_8/95_override_one_or_more_array_elements.rb
+++ section_8/95_override_one_or_more_array_elements.rb
@@ -1,4 +1,9 @@
-days = %w[monday tuesday wednesday thursday friday]
+days = %w[monday tuesday wednesday thursday friday saturday sunday]
 
-days[0] = 'sunday'
+# days[0] = 'sunday'
+
+days[1, 2] = %w[sunday monday]
+p days
+
+days[0..2] = %w[saturday sunday monday]
 p days
