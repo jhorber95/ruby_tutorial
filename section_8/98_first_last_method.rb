075558a12792371272718314027c67d3357ee187
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-08 14:44:12 -0500

    the custom first and custom last methods on an array

--- section_8/98_first_last_method.rb
+++ section_8/98_first_last_method.rb
@@ -2,3 +2,19 @@ numbers = [12, 2, 5, 4, 8, 5, 5, 2, 2, 5, 8]
 
 p numbers.first(2)
 p numbers.last(3)
+
+def custom_first(arr, num = 0)
+  return arr[0] if num.zero?
+
+  arr[0, num]
+end
+
+def custom_last(arr, num = 0)
+  return arr[-1] if num.zero?
+
+  arr[-num..-1]
+end
+puts
+
+p custom_first(numbers, 2)
+p custom_last(numbers, 2)
