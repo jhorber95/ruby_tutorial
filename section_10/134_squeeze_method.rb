87dca434be8978ab8f3912c2802dd6adacded878
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-09 09:55:23 -0500

    squeeze method on a string

---
+++ section_10/134_squeeze_method.rb
@@ -0,0 +1,6 @@
+sentence = 'thee bestt plaaayer       inn the worllld'
+
+# this method remove duplicate character
+# if I only want to focus on something, I pass
+# a character like parameter
+p sentence.squeeze # the best player in the world
