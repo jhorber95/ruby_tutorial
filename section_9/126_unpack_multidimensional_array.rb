7bbb3283697a5e0b67e78103377e6b22ef960a6f
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-09 09:56:12 -0500

    unpack multidimensional array

--- section_9/126_unpack_multidimensional_array.rb
+++ section_9/126_unpack_multidimensional_array.rb
@@ -4,3 +4,16 @@ alberto, lucia, maria = users
 p alberto
 p lucia
 p maria
+
+puts
+names = 'maria alberto gomez tatiana fernando'
+arr_name = names.split
+
+max = arr_name[0]
+
+arr_name.each do |item|
+  max = item if item.length > max.length
+end
+
+p max
+p 'maria'.length
