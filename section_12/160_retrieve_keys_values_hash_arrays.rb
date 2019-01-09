0fe16bc8c7138d81e6cfe4dde46c9b5e2b062807
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-09 17:15:25 -0500

    the sort and sort_by method on a hash

--- section_12/160_retrieve_keys_values_hash_arrays.rb
+++ section_12/160_retrieve_keys_values_hash_arrays.rb
@@ -12,6 +12,8 @@ person.default = 'not found'
 puts
 p person[:last_name]
 puts
+
+
 # video 162
 # Convert hash to array and vice versa
 arr = person.to_a
@@ -19,3 +21,15 @@ p arr
 
 new_hash_person = arr.to_h
 p new_hash_person
+
+# video 163
+# the sort an sort_by methods on a hash
+puts
+
+puts ' sort and sort_by'
+p person.sort
+p person.sort.reverse
+
+puts
+p person.sort_by { |key, value| key}
+p person.sort_by { |key, value| value}
