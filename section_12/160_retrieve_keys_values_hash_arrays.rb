247f35356fc644c10610de327c4f3a26fccaf9e8
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-09 17:09:41 -0500

    convert hash to array an vice versa

--- section_12/160_retrieve_keys_values_hash_arrays.rb
+++ section_12/160_retrieve_keys_values_hash_arrays.rb
@@ -11,3 +11,11 @@ person.default = 'not found'
 
 puts
 p person[:last_name]
+puts
+# video 162
+# Convert hash to array and vice versa
+arr = person.to_a
+p arr
+
+new_hash_person = arr.to_h
+p new_hash_person
