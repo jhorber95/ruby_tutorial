31c6977820bb8245d40bc04b663581069fb9f5aa
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 14:38:18 -0500

    convert time object to other objects

--- section_15/185_the_comparable_method_on_time_object.rb
+++ section_15/185_the_comparable_method_on_time_object.rb
@@ -1,5 +1,18 @@
 date_1 = Time.new(2016, 5, 12)
 date_2 = Time.new(2018, 5, 12)
+date_3 = Time.new(2017, 3, 15)
 
 p date_1 < date_2
 p date_1 > date_2
+
+p date_3.between?(date_1, date_2)
+
+#  video 186
+# convert time object to other object
+puts
+puts
+
+someday = Time.new(2017, 3, 15)
+puts someday.to_s
+puts someday.ctime
+p someday.to_a
