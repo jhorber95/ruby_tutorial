13bcceac359ac5449661543e76934b49e498a595
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 16:01:40 -0500

    the scan methos

--- section_17/197_intro_to_regular_expression.rb
+++ section_17/197_intro_to_regular_expression.rb
@@ -1,2 +1,7 @@
 phrase = 'the ruby programming language is amazing'
 p phrase =~ /t/ # return the index position of the frs match
+
+# video 198
+# the scan method part 1
+p phrase.scan(/ing/)
+p phrase.scan(/[uy]/) # pass multiple character
