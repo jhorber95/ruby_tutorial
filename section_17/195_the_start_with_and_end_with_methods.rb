2dee085fcb7933b5b254fa5cd143a31bbae7f2d6
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 15:42:09 -0500

    the include? method

--- section_17/195_the_start_with_and_end_with_methods.rb
+++ section_17/195_the_start_with_and_end_with_methods.rb
@@ -3,3 +3,10 @@ phrase = 'the ruby programming language is amazing'
 puts phrase.start_with?('the')
 puts phrase.end_with?('ing')
 puts phrase.end_with?(' ing')
+
+#  video 196
+# the include? method
+puts
+puts
+
+puts phrase.include?('lang')
