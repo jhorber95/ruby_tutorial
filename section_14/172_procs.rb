4243b3a77871002434bdcd6ef2204e401c1ada89
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 10:13:14 -0500

    refactoring procs

--- section_14/172_procs.rb
+++ section_14/172_procs.rb
@@ -12,3 +12,14 @@ a_cubes, b_cubes, c_cubes = [a, b, c].map { |array| array.map(&cubes) }
 p a_cubes
 p b_cubes
 p c_cubes
+
+puts
+
+
+is_old = Proc.new do |age|
+  age > 40
+end
+ages = [50, 23, 41, 32, 98]
+
+p ages.select(&is_old)
+p ages.reject(&is_old)
