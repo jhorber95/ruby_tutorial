127fb183b45047f4b78148ae50b75edf7d5679f6
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 16:28:17 -0500

    special character backspace

--- section_17/197_intro_to_regular_expression.rb
+++ section_17/197_intro_to_regular_expression.rb
@@ -1,11 +1,14 @@
 phrase = 'the ruby programming language is amazing 11-22-32--5555'
-p phrase =~ /t/ # return the index position of the frs match
+p phrase =~ /t/ # return the index position of the first match
 
 # video 198
 # the scan method part 1
 p phrase.scan(/ing/)
 p phrase.scan(/[uy]/) # pass multiple character
 p phrase.scan(/\d/) # return only numbers
+p phrase.scan(/\D/) # return only letters
+p phrase.scan(/\s/) # return any occurrence of a of a space
+p phrase.scan(/\S/) # S mean any non whitespace
 
 puts
 # this mean when we do slash D is one or more
