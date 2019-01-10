cf6fee294130327e495bce56d3fde0fd908782e7
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 14:51:35 -0500

    convert string to Time with custom format

--- section_15/187_convert_time_to_formatting_string.rb
+++ section_15/187_convert_time_to_formatting_string.rb
@@ -1,3 +1,9 @@
 today = Time.now
 
-p today.strftime("%B %d, %Y")
+p today.strftime('%B %d, %Y')
+puts
+puts
+require 'time'
+
+puts Time.parse('2018-02-28')
+puts Time.strptime('03-12-2015', '%m-%d-%Y') # custom format to convert
