3ca073873bfd73a990c0f11958a6476689764158
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 15:15:21 -0500

    require_relative method

--- section_16/193_part_1_load_method.rb
+++ section_16/193_part_1_load_method.rb
@@ -4,3 +4,5 @@ load '193_part_2_load_method.rb'
 require './193_part_2_load_method'
 require './193_part_2_load_method'
 require './193_part_2_load_method'
+
+require_relative '193_part_2_load_method'
