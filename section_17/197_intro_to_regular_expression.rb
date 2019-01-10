0da95fdbb6ce107a8ba5c66cf8f3f535677aee4a
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 16:10:28 -0500

    the scan method

--- section_17/197_intro_to_regular_expression.rb
+++ section_17/197_intro_to_regular_expression.rb
@@ -1,7 +1,17 @@
-phrase = 'the ruby programming language is amazing'
+phrase = 'the ruby programming language is amazing 11-22-32--5555'
 p phrase =~ /t/ # return the index position of the frs match
 
 # video 198
 # the scan method part 1
 p phrase.scan(/ing/)
 p phrase.scan(/[uy]/) # pass multiple character
+p phrase.scan(/\d/) # return only numbers
+
+puts
+# this mean when we do slash D is one or more
+# ocurrences of a digit in a row
+p phrase.scan(/\d+/)
+
+# every time we run into something that matches
+# from the rejects condition, do something
+phrase.scan(/\d+/) {|digit_match| puts digit_match.length}
