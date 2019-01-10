9ba26fdb9f6dea91c229ffb157f4bdd41f84dd02
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 14:58:05 -0500

    write a text file

--- section_16/189_read_txt_file.rb
+++ section_16/189_read_txt_file.rb
@@ -1,3 +1,11 @@
 File.open('read_file.txt').each do |line|
   puts line
 end
+
+
+# video 190
+# write file
+File.open('read_file.txt', 'w') do |file|
+  file.puts 'creating new line'
+  file.write 'ruby on rails'
+end
