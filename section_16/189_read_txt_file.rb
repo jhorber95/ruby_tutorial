0fbb6328ca0b6b72d80f466724fefdd07be3f05c
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-10 15:02:39 -0500

    rename and delete a file

--- section_16/189_read_txt_file.rb
+++ section_16/189_read_txt_file.rb
@@ -10,3 +10,10 @@ File.open('read_file.txt', 'w') do |file|
   file.puts 'creating new line'
   file.write 'ruby on rails'
 end
+
+
+# video 191
+# rename and delete a file
+
+File.rename('old_title.txt', 'new_title.txt')
+File.delete('new_title.txt')
