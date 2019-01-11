aa3af046eab14e42c6bd4ed2161191273206e649
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-11 12:20:23 -0500

    override method

--- section_18/206_intrp_to_classes.rb
+++ section_18/206_intrp_to_classes.rb
@@ -8,5 +8,14 @@ class Gadget
   def info
     "Gadget #{@username} - #{@production_name}"
   end
+
+  # override to_s method
+  def to_s
+    'method override'
+  end
 end
 
+phone = Gadget.new
+
+puts phone.info
+puts phone.to_s
