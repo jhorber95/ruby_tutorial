d6cee67078e124e8d84a6376e9cf2ad6d8ce0b75
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-11 15:19:46 -0500

    import modules into files

---
+++ section_19/222_import_modules.rb
@@ -0,0 +1,3 @@
+require_relative 'conversions.module.rb'
+
+p LengthConversions.centimeters_to_meters(1200)
---
+++ section_19/conversions.module.rb
@@ -0,0 +1,11 @@
+module LengthConversions
+  SENTENCE = 'lorem ipsum dolor simet'
+
+  def self.miles_to_feet(miles)
+    miles * 5200
+  end
+
+  def self.centimeters_to_meters(centimeters)
+    centimeters / 100
+  end
+end
