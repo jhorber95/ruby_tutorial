d72f56dbd1752c04ea73c5ad6136c99e46afc2e6
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-11 14:50:03 -0500

    add parameter to initialize method

--- section_18/216_getter_method.rb
+++ section_18/216_getter_method.rb
@@ -1,17 +1,18 @@
 class Person
   attr_accessor :name, :address, :phone
 
-  def initialize
-    @name = 'Peter'
-    @phone = 31_654
-    @address = 'cr 12# 5-6'
+  def initialize(name, phone, address)
+    @name = name
+    @phone = phone
+    @address = address
   end
 
 end
 
-someone = Person.new
+someone = Person.new('peter', '+57 322 862 0121', 'Cra1c 45#4')
 
-someone.address = 'new address'
-someone.name = 'Jose'
+# someone.address = 'new address'
+# someone.name = 'Jose'
 p someone.name
 p someone.address
+p someone.phone
