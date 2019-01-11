72c98173bb8a9cc5efeba1f7fc0cf8deea561738
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-11 16:56:51 -0500

    ancestor method and inheritance

--- section_19/226_ancestors_method.rb
+++ section_19/226_ancestors_method.rb
@@ -6,6 +6,10 @@ end
 
 class GoodMorning
   include Greet
+
+  def greeting(name)
+    "hi, #{name} from GoodMorning class"
+  end
 end
 
 class GoodAfternoon
@@ -16,9 +20,13 @@ class GreetingAllDay < GoodMorning
 end
 
 greet_morning = GoodMorning.new
+greet_afternoon = GoodAfternoon.new
 greeting_all_day = GreetingAllDay.new
 
 p GreetingAllDay.ancestors
 
+puts
+
 p greet_morning.greeting('Alberth')
 p greeting_all_day.greeting('Maria')
+p greet_afternoon.greeting('Jhon')
