0fbaca5e4522eb5c0783e181c353ba1ce3554973
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-14 09:41:45 -0500

    monkey patching String class

--- section_20/236_monkey_patching.rb
+++ section_20/236_monkey_patching.rb
@@ -14,4 +14,20 @@ class Array
   end
 end
 
+class String
+  def alphabet_sum
+    alphabet = ('a'..'z').to_a
+    sum = 0
+    downcase.each_char do |character|
+      if alphabet.include?(character)
+        numeric_index = alphabet.index(character) + 1
+        sum += numeric_index
+      end
+    end
+    sum
+  end
+end
 p [1, 'hi', 5, true, 5, nil].sum
+
+# sum 1 + 2 + 3
+p 'abc'.alphabet_sum
