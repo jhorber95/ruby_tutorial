4db2e13f20629caf3be2b3eb47f3397ff751232b
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-14 09:53:01 -0500

    monkey patching Integer class

--- section_20/237_mokey_patch_hash_class.rb
+++ section_20/237_mokey_patch_hash_class.rb
@@ -12,3 +12,30 @@ end
 scores = {a: 100, b: 200, c: 100, d: 200, e: 320, f: 320}
 
 p scores.identify_duplicate_values
+
+puts
+
+class Integer
+  def seconds
+    self
+  end
+
+  def minutes
+    self * 60
+  end
+
+  def hours
+    self * 60 * 60
+  end
+
+  def days
+    self * 60 * 60 * 24
+  end
+end
+
+puts Time.now
+puts Time.now + 45.minutes
+puts Time.now + 2.hours
+puts Time.now + 2.days
+
+
